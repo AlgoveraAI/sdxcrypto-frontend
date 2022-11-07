a84fb0c9e9e824f5813fa699f599ea44c6c02199
Barefoot-Dev <barefootdev@proton.me>
2022-11-07 13:52:36 +0800

    add primary algovera color

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,8 +5,14 @@ module.exports = {
     "./pages/**/*.{js,ts,jsx,tsx}",
     "./components/**/*.{js,ts,jsx,tsx}",
   ],
-  theme: {
-    extend: {},
-  },
   plugins: [],
+  theme: {
+    extend: {
+      colors: {
+        primary: "#1937D6", // https://www.color-hex.com/color/1937d6
+        "primary-darker": "#112695",
+        "primary-lighter": "#465ede",
+      },
+    },
+  },
 };
