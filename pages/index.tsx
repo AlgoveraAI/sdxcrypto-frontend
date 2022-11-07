2861c57a6a121e94a8fb150de340a3d764de1037
Barefoot-Dev <barefootdev@proton.me>
2022-11-07 15:28:15 +0800

    setup credits modal

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,10 +2,12 @@ import Nav from "../components/nav";
 import Hero from "../components/hero";
 import HowItWorks from "../components/howitworks";
 import Pricing from "../components/pricing";
+import CreditsModal from "../components/credits-modal";
 
 export default function Home() {
   return (
     <div>
+      <CreditsModal />
       <Nav />
       <Hero />
       <HowItWorks />
