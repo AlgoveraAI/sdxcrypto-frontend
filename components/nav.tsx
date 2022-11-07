b40a678d406faa3ea727b4528b296706a50b9b5b
Barefoot-Dev <barefootdev@proton.me>
2022-11-07 13:21:47 +0800

    update colors of mobile nav menu

--- components/nav.tsx
+++ components/nav.tsx
@@ -52,7 +52,7 @@ export default function Nav() {
             </a>
           </div>
           <div className="-my-2 -mr-2 md:hidden">
-            <Popover.Button className="inline-flex items-center justify-center rounded-md p-2 text-gray-300 hover:bg-gray-100 hover:text-gray-500 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-indigo-500">
+            <Popover.Button className="inline-flex items-center justify-center rounded-md p-2 text-gray-300 hover:text-gray-500 focus:outline-none">
               <span className="sr-only">Open menu</span>
               <Bars3Icon className="h-6 w-6" aria-hidden="true" />
             </Popover.Button>
@@ -60,13 +60,13 @@ export default function Nav() {
           <Popover.Group as="nav" className="hidden space-x-10 md:flex">
             <a
               href="#"
-              className="text-base font-medium text-gray-300 hover:text-gray-400"
+              className="text-base font-medium text-gray-300 hover:text-gray-400 focus:outline-none"
             >
               About Us
             </a>
             <a
               href="#"
-              className="text-base font-medium text-gray-300 hover:text-gray-400"
+              className="text-base font-medium text-gray-300 hover:text-gray-400 focus:outline-none"
             >
               How it Works
             </a>
@@ -155,19 +155,19 @@ export default function Nav() {
           focus
           className="absolute inset-x-0 top-0 origin-top-right transform p-2 transition md:hidden"
         >
-          <div className="divide-y-2 divide-gray-50 rounded-lg bg-white shadow-lg ring-1 ring-black ring-opacity-5">
+          <div className="divide-y-2 divide-gray-50 rounded-lg bg-gray-800 shadow-lg ring-1 ring-black ring-opacity-5">
             <div className="space-y-6 py-6 px-5">
               <div className="grid grid-cols-2 gap-y-4 gap-x-8">
                 <a
                   href="#"
-                  className="text-base font-medium text-gray-900 hover:text-gray-700"
+                  className="text-base font-medium text-gray-300 hover:text-gray-400"
                 >
                   About Us
                 </a>
 
                 <a
                   href="#"
-                  className="text-base font-medium text-gray-900 hover:text-gray-700"
+                  className="text-base font-medium text-gray-300 hover:text-gray-400"
                 >
                   How it works
                 </a>
@@ -175,7 +175,7 @@ export default function Nav() {
                   <a
                     key={item.name}
                     href={item.href}
-                    className="text-base font-medium text-gray-900 hover:text-gray-700"
+                    className="text-base font-medium text-gray-300 hover:text-gray-400"
                   >
                     {item.name}
                   </a>
@@ -184,7 +184,7 @@ export default function Nav() {
               <div>
                 <a
                   href="#"
-                  className="flex w-full items-center justify-center rounded-md border border-solid border-indigo-500 px-4 py-2 text-base font-medium text-white shadow-sm hover:bg-indigo-700"
+                  className="flex w-full items-center justify-center rounded-md border border-solid border-indigo-500 px-4 py-2 text-base font-medium text-indigo-500 shadow-sm hover:text-indigo-700"
                 >
                   Connect Wallet
                 </a>
