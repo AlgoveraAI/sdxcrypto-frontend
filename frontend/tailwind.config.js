3ba571aa54c0241896cb367dfde6fbac790dfd22
Barefoot-Dev <barefootdev@proton.me>
2022-11-18 09:54:52 +0800

    attempt action fix

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -14,7 +14,7 @@ module.exports = {
       colors: {
         primary: "#1937D6", // https://www.color-hex.com/color/1937d6
         "primary-darker": "#112695",
-        "primary-lighter": "#465ede",
+        "primary-lighter": "#5771f2",
         background: "#0f172a",
       },
     },
