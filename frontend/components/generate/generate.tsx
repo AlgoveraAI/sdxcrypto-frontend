a6976684ed8ced8f31c6442530e0b5bf1f1359a7
Barefoot-Dev <barefootdev@proton.me>
2022-11-28 09:41:00 +0800

    switch to runpod

--- frontend/components/generate/generate.tsx
+++ frontend/components/generate/generate.tsx
@@ -58,16 +58,16 @@ export default function Generate({
       baseModel = "stable-diffusion-v1-5";
     }
 
-    const res = await fetch("/api/banana", {
+    const res = await fetch("/api/txt2img", {
       method: "POST",
       body: JSON.stringify({
         uid: user.uid,
         prompt: prompt,
-        // base_model: baseModel,
-        // height: resolution,
-        // width: resolution,
-        // inf_steps: inferenceSteps,
-        // guidance_scale: guidanceScale,
+        base_model: baseModel,
+        height: resolution,
+        width: resolution,
+        inf_steps: inferenceSteps,
+        guidance_scale: guidanceScale,
       }),
     });
     // todo handle out of credits error
