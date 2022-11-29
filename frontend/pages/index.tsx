9c4add2201cf4fa4f0fad67121ba2073147cc878
Barefoot-Dev <barefootdev@proton.me>
2022-11-29 11:43:37 +0800

    rm test error

--- frontend/pages/index.tsx
+++ frontend/pages/index.tsx
@@ -19,14 +19,6 @@ const Home: NextPage<PageProps> = ({
 
   return (
     <div>
-      <button
-        type="button"
-        onClick={() => {
-          throw new Error("Sentry Frontend Error");
-        }}
-      >
-        Throw error
-      </button>
       <Hero />
       <HowItWorks howitworksRef={howitworksRef} />
       <Pricing
