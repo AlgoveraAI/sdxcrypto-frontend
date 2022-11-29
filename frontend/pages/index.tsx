8175a71799f9e988ccee4016c6aa307a7b4b7c3b
Barefoot-Dev <barefootdev@proton.me>
2022-11-29 11:23:56 +0800

    re-attempty sentry

--- frontend/pages/index.tsx
+++ frontend/pages/index.tsx
@@ -19,6 +19,14 @@ const Home: NextPage<PageProps> = ({
 
   return (
     <div>
+      <button
+        type="button"
+        onClick={() => {
+          throw new Error("Sentry Frontend Error");
+        }}
+      >
+        Throw error
+      </button>
       <Hero />
       <HowItWorks howitworksRef={howitworksRef} />
       <Pricing
---
+++ frontend/sentry.server.config.js
@@ -0,0 +1,17 @@
+// This file configures the initialization of Sentry on the server.
+// The config you add here will be used whenever the server handles a request.
+// https://docs.sentry.io/platforms/javascript/guides/nextjs/
+
+import * as Sentry from '@sentry/nextjs';
+
+const SENTRY_DSN = process.env.SENTRY_DSN || process.env.NEXT_PUBLIC_SENTRY_DSN;
+
+Sentry.init({
+  dsn: SENTRY_DSN || 'https://8e9ad370193244218dc754f031266686@o4504240240328704.ingest.sentry.io/4504240241377280',
+  // Adjust this value in production, or use tracesSampler for greater control
+  tracesSampleRate: 1.0,
+  // ...
+  // Note: if you want to override the automatic release value, do not set a
+  // `release` value here - use the environment variable `SENTRY_RELEASE`, so
+  // that it will also get attached to your source maps
+});
