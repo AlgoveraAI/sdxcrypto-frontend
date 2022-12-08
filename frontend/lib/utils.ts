525e93e1da595cd1188a484f1fc09e4a9038584c
Barefoot-Dev <barefootdev@proton.me>
2022-12-08 11:18:44 +0800

    suppress logs on main

--- frontend/lib/utils.ts
+++ frontend/lib/utils.ts
@@ -1,6 +1,8 @@
 const { execSync } = require("child_process");
 
 export function getCurrentGitBranch() {
+  // NB this only works running on the server (e.g. an API route)
+  // does not work in the browser!
   const branch = execSync("git branch --show-current").toString().trim();
   return branch;
 }
