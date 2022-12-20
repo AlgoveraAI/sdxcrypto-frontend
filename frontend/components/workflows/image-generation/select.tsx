0ae4c51b447f965004e37f987b7b9aeaa7ba9024
Barefoot-Dev <barefootdev@proton.me>
2022-12-21 00:31:41 +0800

    dalle

--- frontend/components/workflows/image-generation/select.tsx
+++ frontend/components/workflows/image-generation/select.tsx
@@ -2,23 +2,26 @@ import Image from "next/image";
 import { ArrowTopRightOnSquareIcon } from "@heroicons/react/20/solid";
 
 import stabilityLogo from "../../../assets/workflows/stability.jpeg";
+import openaiLogo from "../../../assets/workflows/openai.jpg";
 
 const models = [
   {
-    name: "Stable Diffusion (v1.5)",
+    name: "Stable Diffusion",
+    id: "stable",
     description:
       "A latent text-to-image diffusion model capable of generating photo-realistic images given any text input, cultivates autonomous freedom to produce incredible imagery, empowers billions of people to create stunning art within seconds.",
     website: "https://github.com/CompVis/stable-diffusion",
     image: stabilityLogo,
-    credits_per_use: 1,
+    credits_per_use: 2,
   },
   {
-    name: "Stable Diffusion (v2)",
+    name: "DALL·E 2",
+    id: "dalle",
     description:
-      "A latent text-to-image diffusion model capable of generating photo-realistic images given any text input, cultivates autonomous freedom to produce incredible imagery, empowers billions of people to create stunning art within seconds.",
-    website: "https://github.com/CompVis/stable-diffusion",
-    image: stabilityLogo,
-    credits_per_use: 1,
+      "DALL·E 2 is a new AI system that can create realistic images and art from a description in natural language.",
+    website: "https://openai.com/dall-e-2/",
+    image: openaiLogo,
+    credits_per_use: 2,
   },
 ];
 
